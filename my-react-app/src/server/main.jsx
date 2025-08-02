e2d18e944576ae7bd6be1d6442e41152cc302716
Dharmraj06 <vagheladharmraj06@gmail.com>
2025-08-02 15:09:05 +0530

    added css for newnote

--- my-react-app/src/server/main.jsx
+++ my-react-app/src/server/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import '../client/public/login.css';
 import '../client/public/dashboard.css';
 import '../client/public/footer.css';
+import '../client/public/newNote.css'
 
 createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
